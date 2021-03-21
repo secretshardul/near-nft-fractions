85e19da25c3aff9746deda054362cb3b7204cd4d
shardul <shardul.aeer@somaiya.edu>
2021-03-21 15:35:54 +0530

    Test cases for NFT contract
    Mint NFT, find owner

--- src/near/nftContract.ts
+++ src/near/nftContract.ts
@@ -3,16 +3,22 @@ const {
     Near, Contract, KeyPair,
     keyStores: { InMemoryKeyStore },
 } = nearAPI
-import * as credentials from './dev-1615619158857-5968612.json'
+import * as credentials from './dev-1616311842543-1234040.json'
 
 const networkId = 'default'
-const contractName = 'dev-1616312075136-6446253' // NFT contract
+const contractName = credentials.account_id
 const nodeUrl = 'https://rpc.testnet.near.org'
+
+export interface NftContract extends nearAPI.Contract {
+    get_token_owner: Function,
+    mint_to: Function,
+}
 
 export default async function getNftContract () {
     // Initialize Near
     const keyStore = new InMemoryKeyStore()
     await keyStore.setKey(networkId, contractName, KeyPair.fromString(credentials.private_key))
+    console.log(keyStore)
 
     const near = new Near({
         networkId, nodeUrl,
@@ -25,5 +31,5 @@ export default async function getNftContract () {
     return new Contract(account, contractName, {
         viewMethods: ['get_token_owner'],
         changeMethods: ['mint_to'],
-    })
+    }) as NftContract
 }
--- tests/nftContract.test.ts
+++ tests/nftContract.test.ts
@@ -1,5 +1,25 @@
 import getNftContract from '../src/near/nftContract'
+import { assert } from 'chai'
 
 describe('Contract test', async () => {
-    const contract = await getNftContract()
+
+    it('Mint NFT', async () => {
+        const contract = await getNftContract()
+        const saveAddressResp = await contract.mint_to({
+            owner_id: 'monkeyis.near'
+        })
+        console.log('Mint NFT response', saveAddressResp)
+
+        assert(true)
+    })
+
+    it('Get NFT owner', async () => {
+        const contract = await getNftContract()
+        const getOwnerResp = await contract.get_token_owner({
+            token_id: '1'
+        })
+        console.log('Get owner response', getOwnerResp)
+
+        assert(true)
+    })
 })
