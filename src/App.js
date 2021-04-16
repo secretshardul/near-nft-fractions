981292692d3e15f7e08300e4c46a8ae614eacc5e
shardul <shardul.aeer@somaiya.edu>
2021-04-16 14:47:07 +0530

    doc: Working plan

--- src/App.js
+++ src/App.js
@@ -176,33 +176,7 @@ export default function App() {
             setShowNotification(false)
           }, 11000)
         }}>
-          <fieldset id="fieldset">
-            <label
-              htmlFor="greeting"
-              style={{
-                display: 'block',
-                color: 'var(--gray)',
-                marginBottom: '0.5em'
-              }}
-            >
-              Change greeting
-            </label>
-            <div style={{ display: 'flex' }}>
-              <input
-                autoComplete="off"
-                defaultValue={greeting}
-                id="greeting"
-                onChange={e => setButtonDisabled(e.target.value === greeting)}
-                style={{ flex: 1 }}
-              />
-              <button
-                disabled={buttonDisabled}
-                style={{ borderRadius: '0 5px 5px 0' }}
-              >
-                Save
-              </button>
-            </div>
-          </fieldset>
+
         </form>
         <h2>1. Lets get an NFT</h2>
         <button onClick={mintToken}>Mint NFT</button>
